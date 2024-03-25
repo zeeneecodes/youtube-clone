58a8dc24cfda8904a8dbde2ac6749ea95139a39c
zeenee <cabrerarizzi@gmail.com>
2024-03-25 19:14:31 +0800

    add all video data to videos.js

---
+++ data/videos.js
@@ -0,0 +1,69 @@
+export const videoDetails = [
+  {
+    channelName:"ezlilyy",
+    channelLink: "https://www.youtube.com/@ezlilyy",
+    channelPicture: "Images/Channel Pictures/channel-1.jpeg",
+    thumbnail: "Images/thumbnails/thumbnail-1.webp",
+    title:"Checking Out The BRAND NEW Festivals 🏜️",
+    duration:"3:15:16",
+    views:"5.8K",
+    publishedWhen:"Streamed 1 day ago",
+    originalLink: "https://www.youtube.com/watch?v=kZ0-eF9xY28",
+    isVerified: "No"
+  }, {
+    channelName:"Kara and Nate",
+    channelLink: "https://www.youtube.com/@KaraandNate",
+    channelPicture: "Images/Channel Pictures/channel-2.jpeg",
+    thumbnail: "Images/thumbnails/thumbnail-2.webp",
+    title:"95 hours on Canadas BEST train! (Vancouver to Toronto on Via Rail)",
+    duration:"32:54",
+    views:"107K",
+    publishedWhen:"3 hours ago",
+    originalLink: "https://www.youtube.com/watch?v=G9Xh95FO0i4",
+    isVerified: "Yes"
+  }, {
+    channelName:"Sandristas 009",
+    channelLink: "https://www.youtube.com/@sandristas009",
+    channelPicture: "Images/Channel Pictures/channel-3.jpeg",
+    thumbnail: "Images/thumbnails/thumbnail-3.webp",
+    title:"Playlist for Smooth Brained Organisms",
+    duration:"44:22",
+    views:"1M",
+    publishedWhen:"3 months ago",
+    originalLink: "https://www.youtube.com/watch?v=1rd4P7uMvvQ",
+    isVerified: "No"
+  }, {
+    channelName:"Ashikai",
+    channelLink: "https://www.youtube.com/@Ashikai",
+    channelPicture: "Images/Channel Pictures/channel-4.jpeg",
+    thumbnail: "Images/thumbnails/thumbnail-4.webp",
+    title:"[4.5] Xamaran, Paimon, And Eating Things You're Not Supposed To - A...",
+    duration:"43:52",
+    views:"75K",
+    publishedWhen:"2 days ago",
+    originalLink: "https://www.youtube.com/watch?v=YHCv8KFdL6M",
+    isVerified: "Yes"
+  }, {
+    channelName:"PeruseProject",
+    channelLink: "https://www.youtube.com/@Ashikai",
+    channelPicture: "Images/Channel Pictures/channel-4.jpeg",
+    thumbnail: "Images/thumbnails/thumbnail-4.webp",
+    title:"I finally found a slow burn romantasy book!! and I liked it!!",
+    duration:"35:42",
+    views:"31K",
+    publishedWhen:"9 days ago",
+    originalLink: "https://www.youtube.com/watch?v=GqMQLP6N25g",
+    isVerified: "Yes"
+  }, {
+    channelName:"The Rain Driver",
+    channelLink: "https://www.youtube.com/@TheRainDriver",
+    channelPicture: "Images/Channel Pictures/channel-4.jpeg",
+    thumbnail: "Images/thumbnails/thumbnail-4.webp",
+    title:'Driving in Rain for Sleeping "Real Footage" Rain Noise Highway,...',
+    duration:"8:01:22",
+    views:"322K",
+    publishedWhen:"1 year ago",
+    originalLink: "https://www.youtube.com/watch?v=GV4TNIiJQQU",
+    isVerified: "No"
+  }
+];
