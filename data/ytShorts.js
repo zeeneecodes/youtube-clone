8e4a6b817b18e7e06810022f4c27bb872545cc62
zeenee <cabrerarizzi@gmail.com>
2024-03-30 21:16:50 +0800

    add youtube shorts data

---
+++ data/ytShorts.js
@@ -0,0 +1,27 @@
+export const ytShortsData = [
+  {
+    title: 'Pikachu | HTML CSS JavaScript',
+    thumbnail: 'Images/thumbnails/yt-short-1.webp',
+    views: '234K'
+  }, {
+    title: 'Condition: talkative, contemplative, kissable,...',
+    thumbnail: 'Images/thumbnails/yt-short-2.webp',
+    views: '2.2M'
+  }, {
+    title: 'Off-leash Heeling Pro Tips',
+    thumbnail: 'Images/thumbnails/yt-short-3.webp',
+    views: '63K'
+  }, {
+    title: 'A Wild Nvidia RTX 4090 Super Appears...',
+    thumbnail: 'Images/thumbnails/yt-short-4.webp',
+    views: '20K'
+  }, {
+    title: 'Kindergarten is feral 😅 #teachers #teaching...',
+    thumbnail: 'Images/thumbnails/yt-short-5.webp',
+    views: '6.2M'
+  },{
+    title: 'It all makes sense now 😅',
+    thumbnail: 'Images/thumbnails/yt-short-6.webp',
+    views: '941K'
+  }
+];
